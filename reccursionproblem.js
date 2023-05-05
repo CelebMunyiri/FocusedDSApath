25b7624cf670f5fffb79c62b7940719cddd45873
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-05 05:10:42 +0300

    Recursion problems and solutions

---
+++ reccursionproblem.js
@@ -0,0 +1,38 @@
+"usse strict"
+//Create a function that finds the factorial of a number
+/*
+function factor(number){
+    if(number===1) return 1
+    
+return number*factor(number-1)
+}
+console.log(factor(7))
+
+//Create a function that finds the power of a number
+function power(base,exponent){
+    //base case
+    if(exponent===0) {
+        return 1}
+    else{
+        //recursive case
+        return base*power(base,exponent-1)
+    }
+
+}
+console.log(power(6,2))
+//Function to find product of an array
+
+function product(arr){
+    //base case
+    if(arr.length===0){
+        return 1
+    } 
+    //recursive case
+    else{
+        return arr[0]*product(arr.slice(1))
+    }
+}
+console.log(product([1,2,3]))*/
+
+
+
