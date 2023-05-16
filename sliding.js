3184b1dd2c3db5658517bc38861baf0560924a07
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-16 10:04:28 +0300

    Sliding window
    This file demonstrates the implementation of a sliding window in a problem

---
+++ sliding.js
@@ -0,0 +1,18 @@
+'use strict';
+//practise on the sliding window algorithm
+//Its a technique to increase the efficiency of any problem which can fit into it's algorithm
+
+//problem:FOR AN ARRAY WE NEED TO FIND THE MAX SUM OF P CONSECUTIVE ELEMENTS
+
+const array=[2,4,6,1,-2,9,0]; //length=7, p=3
+//we will be using two pointers i and j
+const findSum=(arr)=>{
+    let maxSum=0
+    
+   for(let i=0;i<=n-package;i++){
+    let sum=0
+    for(let j=i;j<i+p;j++){
+
+    }
+   }
+}
