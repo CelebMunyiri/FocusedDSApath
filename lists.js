b242fd1963c76aa80f71d103f77ba5388f6cb209
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-16 10:03:22 +0300

    intro to linked lists generic concepts
    This file has general concept of what linked lists are without classifying them in either singly or doubly linked lists

---
+++ lists.js
@@ -0,0 +1,11 @@
+'use strict';
+//in this file we are going to learn limked lists
+//the learning starts here
+
+//reference to next Node
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null
+    }
+}
