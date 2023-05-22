82b963ad9d6551637f6b241a2b15cd4e2e745221
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-22 11:57:40 +0300

    optimized solution for base 7 problem

--- base.js
+++ base.js
@@ -1,8 +1,9 @@
 'use strict';
 //The problem
-//gic=ven an integer, num, return its base seven represention as a string
+//given an integer, num, return its base seven represention as a string
 //Example num=42, return 60
-//Solution
+//Solution1
+/*
 let num=42;
 const returnBase=function(num){
     let base=num/7
@@ -15,4 +16,25 @@ const returnBase=function(num){
     return String(`${base}0`);
 
 }
-console.log(returnBase(num));
+console.log(returnBase(num));*/
+//Solution2 starts here
+function convertToBase7(num){
+    if(num===0){
+        return 0;
+    }
+    let isNegative=false;
+    if(num<0){
+        num=-num//convert negative number to positive
+    }
+    let result="";
+    while(num>0){
+        result=(num%7)+result;
+        num=Math.floor(num/7)
+    }
+    if(isNegative){
+        result="-"+result;//adding negative sign if the original number was negative 
+    }
+    return result;
+}
+const num=42
+console.log(convertToBase7(num));
